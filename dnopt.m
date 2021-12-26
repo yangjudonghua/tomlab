d10e19b694a3e4d6668e44f305b9428a4209c27f
yangjudonghua <96676599+yangjudonghua@users.noreply.github.com>
2021-12-26 14:43:42 +0800

    Add files via upload

---
+++ dnopt.m
@@ -0,0 +1,10 @@
+%#mex
+
+function varargout = dnopt( varargin )
+
+missingTomlabMex(mfilename('fullpath'),'dnopt')
+% This file is not expected to be run - if it is called, the corresponding
+% MEX file is missing from the Tomlab installation.
+
+end
+
